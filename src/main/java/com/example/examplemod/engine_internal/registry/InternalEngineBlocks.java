27e109070f3cbbb52145becdea5452eac180beea
Stasis, the Shattered <99687421+StainlessStasis@users.noreply.github.com>
2026-07-09 01:58:42 -0400

    fix crash, the game now runs

--- src/main/java/com/example/examplemod/engine_internal/registry/InternalEngineBlocks.java
+++ src/main/java/com/example/examplemod/engine_internal/registry/InternalEngineBlocks.java
@@ -14,19 +14,19 @@ public class InternalEngineBlocks {
     public static final DeferredRegister.Blocks BLOCKS =
             DeferredRegister.createBlocks(ExampleMod.MODID);
 
-    public static final DeferredBlock<ProducerBlock> PRODUCER = BLOCKS.register("producer",
-            () -> new ProducerBlock(BlockBehaviour.Properties.of()
-                    .mapColor(MapColor.METAL).strength(3.5f).requiresCorrectToolForDrops()));
+    public static final DeferredBlock<ProducerBlock> PRODUCER = BLOCKS.registerBlock("producer",
+            ProducerBlock::new,
+            () -> BlockBehaviour.Properties.of().mapColor(MapColor.METAL).strength(3.5f).requiresCorrectToolForDrops());
 
-    public static final DeferredBlock<BeltBlock> BELT = BLOCKS.register("belt",
-            () -> new BeltBlock(BlockBehaviour.Properties.of()
-                    .mapColor(MapColor.STONE).strength(2.0f)));
+    public static final DeferredBlock<BeltBlock> BELT = BLOCKS.registerBlock("belt",
+            BeltBlock::new,
+            () -> BlockBehaviour.Properties.of().mapColor(MapColor.STONE).strength(2.0f));
 
-    public static final DeferredBlock<ConsumerBlock> CONSUMER = BLOCKS.register("consumer",
-            () -> new ConsumerBlock(BlockBehaviour.Properties.of()
-                    .mapColor(MapColor.METAL).strength(3.5f).requiresCorrectToolForDrops()));
+    public static final DeferredBlock<ConsumerBlock> CONSUMER = BLOCKS.registerBlock("consumer",
+            ConsumerBlock::new,
+            () -> BlockBehaviour.Properties.of().mapColor(MapColor.METAL).strength(3.5f).requiresCorrectToolForDrops());
 
-    public static final DeferredBlock<MachineBlock> MACHINE = BLOCKS.register("machine",
-            () -> new MachineBlock(BlockBehaviour.Properties.of()
-                    .mapColor(MapColor.METAL).strength(3.5f).requiresCorrectToolForDrops()));
+    public static final DeferredBlock<MachineBlock> MACHINE = BLOCKS.registerBlock("machine",
+            MachineBlock::new,
+            () -> BlockBehaviour.Properties.of().mapColor(MapColor.METAL).strength(3.5f).requiresCorrectToolForDrops());
 }
