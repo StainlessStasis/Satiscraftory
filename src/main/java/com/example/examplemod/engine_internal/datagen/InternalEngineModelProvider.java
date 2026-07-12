743941d25f3af0797e928c08aa035ad01572b602
Stasis, the Shattered <99687421+StainlessStasis@users.noreply.github.com>
2026-07-11 21:47:15 -0400

    curved belts

--- src/main/java/com/example/examplemod/engine_internal/datagen/InternalEngineModelProvider.java
+++ src/main/java/com/example/examplemod/engine_internal/datagen/InternalEngineModelProvider.java
@@ -40,23 +40,37 @@ public class InternalEngineModelProvider extends ModelProvider {
 
     private void registerBeltModels(BlockModelGenerators blockModels, Block belt) {
         Identifier straightModelId = ExampleMod.id("block/belt_straight");
+        Identifier curvedModelId = ExampleMod.id("block/belt_curved");
+
+        // straight
         Variant forward = new Variant(straightModelId);
         Variant forwardRotated = forward.withYRot(Quadrant.R90);
         Variant backward = forward.withYRot(Quadrant.R180);
         Variant backwardRotated = forward.withYRot(Quadrant.R270);
-
         MultiVariant forwardMulti = new MultiVariant(WeightedList.of(forward));
         MultiVariant forwardRotatedMulti = new MultiVariant(WeightedList.of(forwardRotated));
         MultiVariant backwardMulti = new MultiVariant(WeightedList.of(backward));
         MultiVariant backwardRotatedMulti = new MultiVariant(WeightedList.of(backwardRotated));
 
+        // curved
+        Variant curve0 = new Variant(curvedModelId);
+        Variant curve90 = curve0.withYRot(Quadrant.R90);
+        Variant curve180 = curve0.withYRot(Quadrant.R180);
+        Variant curve270 = curve0.withYRot(Quadrant.R270);
+        MultiVariant curveM0 = new MultiVariant(WeightedList.of(curve0));
+        MultiVariant curveM90 = new MultiVariant(WeightedList.of(curve90));
+        MultiVariant curveM180 = new MultiVariant(WeightedList.of(curve180));
+        MultiVariant curveM270 = new MultiVariant(WeightedList.of(curve270));
+
         blockModels.blockStateOutput.accept(
                 MultiVariantGenerator.dispatch(belt)
                         .with(PropertyDispatch.initial(BeltBlock.SHAPE, BeltBlock.REVERSED)
+                                // straight
                                 .select(BeltShape.NORTH_SOUTH, false, backwardMulti)
                                 .select(BeltShape.NORTH_SOUTH, true, forwardMulti)
                                 .select(BeltShape.EAST_WEST, false, backwardRotatedMulti)
                                 .select(BeltShape.EAST_WEST, true, forwardRotatedMulti)
+
                                 .select(BeltShape.ASCENDING_NORTH, false, backwardMulti)        // TODO: slope model
                                 .select(BeltShape.ASCENDING_NORTH, true, forwardMulti)          // TODO: slope model
                                 .select(BeltShape.ASCENDING_SOUTH, false, backwardMulti)        // TODO: slope model
@@ -65,14 +79,16 @@ public class InternalEngineModelProvider extends ModelProvider {
                                 .select(BeltShape.ASCENDING_EAST, true, forwardRotatedMulti)    // TODO: slope model
                                 .select(BeltShape.ASCENDING_WEST, false, backwardRotatedMulti)  // TODO: slope model
                                 .select(BeltShape.ASCENDING_WEST, true, forwardRotatedMulti)    // TODO: slope model
-                                .select(BeltShape.NORTH_EAST, false, backwardMulti)             // TODO: corner model
-                                .select(BeltShape.NORTH_EAST, true, forwardMulti)               // TODO: corner model
-                                .select(BeltShape.NORTH_WEST, false, backwardMulti)             // TODO: corner model
-                                .select(BeltShape.NORTH_WEST, true, forwardMulti)               // TODO: corner model
-                                .select(BeltShape.SOUTH_EAST, false, backwardMulti)             // TODO: corner model
-                                .select(BeltShape.SOUTH_EAST, true, forwardMulti)               // TODO: corner model
-                                .select(BeltShape.SOUTH_WEST, false, backwardMulti)             // TODO: corner model
-                                .select(BeltShape.SOUTH_WEST, true, forwardMulti)               // TODO: corner model
+
+                                // curved
+                                .select(BeltShape.NORTH_EAST, false, curveM0)
+                                .select(BeltShape.NORTH_EAST, true, curveM0)
+                                .select(BeltShape.NORTH_WEST, false, curveM270)
+                                .select(BeltShape.NORTH_WEST, true, curveM270)
+                                .select(BeltShape.SOUTH_EAST, false, curveM90)
+                                .select(BeltShape.SOUTH_EAST, true, curveM90)
+                                .select(BeltShape.SOUTH_WEST, false, curveM180)
+                                .select(BeltShape.SOUTH_WEST, true, curveM180)
                         )
         );
     }
