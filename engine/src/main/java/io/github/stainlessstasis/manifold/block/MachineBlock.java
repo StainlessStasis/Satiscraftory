472f27d44120062444e6b2ec35711dd89e9732cf
Stasis, the Shattered <99687421+StainlessStasis@users.noreply.github.com>
2026-07-14 23:02:38 -0400

    fix machine model and collision shape

--- engine/src/main/java/io/github/stainlessstasis/manifold/block/MachineBlock.java
+++ engine/src/main/java/io/github/stainlessstasis/manifold/block/MachineBlock.java
@@ -7,13 +7,21 @@ import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.GlobalPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 import org.jspecify.annotations.NonNull;
 
 public class MachineBlock extends AbstractDirectionalFactoryBlock {
+    private static final VoxelShape VISUAL_SHAPE = Shapes.box(-0.125, 0.0, -0.125, 1.125, 1.25, 1.125);
+    private static final VoxelShape COLLISION_SHAPE = Shapes.block();
     private static final MapCodec<MachineBlock> CODEC = simpleCodec(MachineBlock::new);
 
     public MachineBlock(Properties properties) {
@@ -39,5 +47,15 @@ public class MachineBlock extends AbstractDirectionalFactoryBlock {
     protected void affectNeighborsAfterRemoval(@NonNull BlockState state, @NonNull ServerLevel level, @NonNull BlockPos pos, boolean movedByPiston) {
         super.affectNeighborsAfterRemoval(state, level, pos, movedByPiston);
         FactoryNetwork.get(level).removeMachine(GlobalPos.of(level.dimension(), pos));
+    }
+
+    @Override
+    protected @NonNull VoxelShape getShape(@NonNull BlockState state, @NonNull BlockGetter level, @NonNull BlockPos pos, @NonNull CollisionContext context) {
+        return VISUAL_SHAPE;
+    }
+
+    @Override
+    protected VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return COLLISION_SHAPE;
     }
 }
