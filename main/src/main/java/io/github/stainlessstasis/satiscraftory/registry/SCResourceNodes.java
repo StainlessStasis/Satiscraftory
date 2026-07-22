92eec383149a2400434551c9a8d29208ac725e26
Stasis, the Shattered <99687421+StainlessStasis@users.noreply.github.com>
2026-07-22 04:23:58 -0400

    adjust node parameters a bit

--- main/src/main/java/io/github/stainlessstasis/satiscraftory/registry/SCResourceNodes.java
+++ main/src/main/java/io/github/stainlessstasis/satiscraftory/registry/SCResourceNodes.java
@@ -1,5 +1,6 @@
 package io.github.stainlessstasis.satiscraftory.registry;
 
+import net.minecraft.util.valueproviders.TrapezoidInt;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
@@ -11,13 +12,13 @@ public class SCResourceNodes {
     // TODO: custom neo registry
     public static final ResourceNodeType IRON = new ResourceNodeType(
             "iron", Items.RAW_IRON, Blocks.IRON_ORE,
-            UniformInt.of(3, 5), UniformInt.of(1, 3), UniformInt.of(20, 30),
-            200
+            UniformInt.of(3, 5), TrapezoidInt.of(1, 4, 2), UniformInt.of(20, 30),
+            500
     );
     public static final ResourceNodeType COPPER = new ResourceNodeType(
             "copper", Items.RAW_COPPER, Blocks.COPPER_ORE,
-            UniformInt.of(3, 5), UniformInt.of(1, 3), UniformInt.of(20, 30),
-            200
+            UniformInt.of(3, 5), TrapezoidInt.of(1, 4, 2), UniformInt.of(20, 30),
+            500
     );
 
     public static final List<ResourceNodeType> TYPES = List.of(IRON, COPPER);
