535bf2e5b617bf00585d8ad8e21e2ca64ab31f57
Stasis, the Shattered <99687421+StainlessStasis@users.noreply.github.com>
2026-08-03 04:06:42 -0400

    fix items registry from branch merge

--- main/src/main/java/io/github/stainlessstasis/satiscraftory/registry/SCItems.java
+++ main/src/main/java/io/github/stainlessstasis/satiscraftory/registry/SCItems.java
@@ -12,18 +12,20 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 
 public class SCItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Satiscraftory.MODID);
 
     private static final List<DeferredHolder<Item, BlockItem>> FACTORY_BLOCK_ITEMS = new ArrayList<>();
-    private static final List<DeferredHolder<Item, Item>> FACTORY_ITEMS = new ArrayList<>();
+    private static final List<DeferredHolder<Item, ? extends Item>> FACTORY_ITEMS = new ArrayList<>();
 
     public static List<DeferredHolder<Item, BlockItem>> getFactoryBlockItems() {
         return Collections.unmodifiableList(FACTORY_BLOCK_ITEMS);
     }
-    public static List<DeferredHolder<Item, Item>> getFactoryItems() {
+    public static List<DeferredHolder<Item, ? extends Item>> getFactoryItems() {
         return Collections.unmodifiableList(FACTORY_ITEMS);
     }
 
@@ -37,7 +39,7 @@ public class SCItems {
     public static final DeferredItem<Item> SCREWS = registerFactoryItem("screws");
     public static final DeferredItem<Item> COPPER_SHEET = registerFactoryItem("copper_sheet");
     public static final DeferredItem<Item> WIRE = registerFactoryItem("wire");
-    public static final DeferredItem<Item> CABLE = registerFactoryItem("cable");
+    public static final DeferredItem<CableItem> CABLE = registerFactoryItem("cable", CableItem::new);
 
     public static <B extends Block> DeferredItem<BlockItem> registerFactoryBlockItem(String name, Supplier<B> blockSupplier) {
         DeferredItem<BlockItem> item = ITEMS.registerSimpleBlockItem(name, blockSupplier);
@@ -47,6 +49,12 @@ public class SCItems {
 
     public static DeferredItem<Item> registerFactoryItem(String name) {
         DeferredItem<Item> item = ITEMS.registerSimpleItem(name);
+        FACTORY_ITEMS.add(item);
+        return item;
+    }
+
+    public static <I extends Item> DeferredItem<I> registerFactoryItem(String name, Function<Item.Properties, ? extends I> func) {
+        DeferredItem<I> item = ITEMS.registerItem(name, func);
         FACTORY_ITEMS.add(item);
         return item;
     }
