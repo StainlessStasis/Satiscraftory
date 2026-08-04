ce4186032cb9439b449f9cdb8abaa67644713dd9
Stasis, the Shattered <99687421+StainlessStasis@users.noreply.github.com>
2026-08-04 00:52:52 -0400

    fix cable offsets for miner

--- main/src/main/java/io/github/stainlessstasis/satiscraftory/factory_component/miner/MinerBlockEntity.java
+++ main/src/main/java/io/github/stainlessstasis/satiscraftory/factory_component/miner/MinerBlockEntity.java
@@ -59,7 +59,7 @@ public class MinerBlockEntity extends ProducerBlockEntity implements MultiblockC
     private final Vec3 particleOffset;
     private long lastParticleTime = -1L;
 
-    public static final Vec3 CABLE_ANCHOR_LOCAL_OFFSET =  new Vec3(-1.5, 141, -71).scale(1/16f);
+    public static final Vec3 CABLE_ANCHOR_LOCAL_OFFSET = new Vec3(-11, 139, -63.5).scale(1/16f);
     private final Vec3 cableAnchorOffset;
 
     public MinerBlockEntity(BlockPos pos, BlockState state) {
@@ -72,7 +72,7 @@ public class MinerBlockEntity extends ProducerBlockEntity implements MultiblockC
                 ? state.getValue(BlockStateProperties.HORIZONTAL_FACING)
                 : Direction.NORTH;
         this.particleOffset = DirectionalOffset.toWorld(facing, PARTICLE_LOCAL_OFFSET);
-        this.cableAnchorOffset = DirectionalOffset.toWorld(facing, CABLE_ANCHOR_LOCAL_OFFSET);
+        this.cableAnchorOffset = getCableOffset(state, CABLE_ANCHOR_LOCAL_OFFSET);
     }
 
     @Override
