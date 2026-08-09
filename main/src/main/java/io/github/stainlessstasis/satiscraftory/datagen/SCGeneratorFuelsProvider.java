5fe105affc68e5e2c00415df8c4e0e790bf95c57
Stasis, the Shattered <99687421+StainlessStasis@users.noreply.github.com>
2026-08-09 14:08:19 -0400

    add more burnable biomass items

--- main/src/main/java/io/github/stainlessstasis/satiscraftory/datagen/SCGeneratorFuelsProvider.java
+++ main/src/main/java/io/github/stainlessstasis/satiscraftory/datagen/SCGeneratorFuelsProvider.java
@@ -18,23 +18,52 @@ public class SCGeneratorFuelsProvider extends ManifoldGeneratorFuelProvider {
 
     @Override
     protected void addFuels(FuelOutput output) {
-        addBiomassFuel("saplings", ItemTags.SAPLINGS, 2f, output);
-        addBiomassFuel("logs", ItemTags.LOGS_THAT_BURN, 10f, output);
-        addBiomassFuel("wheat", Items.WHEAT, 4f, output);
-        addBiomassFuel("hay_bale", Items.HAY_BLOCK, 30f, output);
+        addBiomassFuelTicks("saplings", ItemTags.SAPLINGS, 100, output);
+        addBiomassFuelTicks("leaves", ItemTags.LEAVES, 20, output);
+        addBiomassFuelTicks("logs", ItemTags.LOGS_THAT_BURN, 300, output);
+        addBiomassFuelTicks("planks", ItemTags.PLANKS, 100, output);
+        addBiomassFuelTicks("sticks", Items.STICK, 20, output);
+        addBiomassFuelTicks("bamboo", Items.BAMBOO, 20, output);
+        addBiomassFuelTicks("scaffolding", Items.SCAFFOLDING, 400, output);
+        addBiomassFuelTicks("wool", ItemTags.WOOL, 100, output);
+        addBiomassFuel("wheat", Items.WHEAT, 2f, output);
+        addBiomassFuel("hay_bale", Items.HAY_BLOCK, 20f, output);
+        addBiomassFuel("carrot", Items.CARROT, 2f, output);
+        addBiomassFuel("potato", Items.POTATO, 2f, output);
+        addBiomassFuel("beetroot", Items.BEETROOT, 2f, output);
+        addBiomassFuel("sugar_cane", Items.SUGAR_CANE, 2f, output);
+        addBiomassFuel("cactus", Items.CACTUS, 2f, output);
+        addBiomassFuel("red_mushroom", Items.RED_MUSHROOM, 2f, output);
+        addBiomassFuel("brown_mushroom", Items.BROWN_MUSHROOM, 2f, output);
+        addBiomassFuel("moss_block", Items.MOSS_BLOCK, 2f, output);
+        addBiomassFuel("kelp", Items.KELP, 1f, output);
+        addBiomassFuelTicks("dried_kelp_block", Items.DRIED_KELP_BLOCK, 4000, output);
+        addBiomassFuel("seagrass", Items.SEAGRASS, 1f, output);
+        addBiomassFuel("vine", Items.VINE, 1f, output);
+        addBiomassFuel("lily_pad", Items.LILY_PAD, 1f, output);
+        addBiomassFuel("sweet_berries", Items.SWEET_BERRIES, 1f, output);
+        addBiomassFuel("glow_berries", Items.GLOW_BERRIES, 1f, output);
+        addBiomassFuel("cocoa_beans", Items.COCOA_BEANS, 1f, output);
+        addBiomassFuel("melon_slice", Items.MELON_SLICE, 1f, output);
+        addBiomassFuel("pumpkin", Items.PUMPKIN, 6f, output);
+        addBiomassFuel("melon", Items.MELON, 6f, output);
     }
 
     private void addBiomassFuel(String id, Item item, float burnSeconds, FuelOutput output) {
+        addBiomassFuelTicks(id, item, Math.round(burnSeconds * 20), output);
+    }
+
+    private void addBiomassFuelTicks(String id, Item item, int ticks, FuelOutput output) {
         output.accept(
                 Satiscraftory.id("biomass_" + id),
-                GeneratorFuel.Data.ofItem(SCGeneratorTypes.BIOMASS, item, Math.round(burnSeconds * 20))
+                GeneratorFuel.Data.ofItem(SCGeneratorTypes.BIOMASS, item, ticks)
         );
     }
 
-    private void addBiomassFuel(String id, TagKey<Item> tag, float burnSeconds, FuelOutput output) {
+    private void addBiomassFuelTicks(String id, TagKey<Item> tag, int ticks, FuelOutput output) {
         output.accept(
                 Satiscraftory.id("biomass_" + id),
-                GeneratorFuel.Data.ofTag(SCGeneratorTypes.BIOMASS, tag, Math.round(burnSeconds * 20))
+                GeneratorFuel.Data.ofTag(SCGeneratorTypes.BIOMASS, tag, ticks)
         );
     }
 }
