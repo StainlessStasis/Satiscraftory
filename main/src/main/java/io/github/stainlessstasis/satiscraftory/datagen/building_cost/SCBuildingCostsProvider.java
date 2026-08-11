b620fac6baffd38e0f054f8c091162864380ea43
Stasis, the Shattered <99687421+StainlessStasis@users.noreply.github.com>
2026-08-11 05:35:21 -0400

    add actual implementations for building costs

--- main/src/main/java/io/github/stainlessstasis/satiscraftory/datagen/building_cost/SCBuildingCostsProvider.java
+++ main/src/main/java/io/github/stainlessstasis/satiscraftory/datagen/building_cost/SCBuildingCostsProvider.java
@@ -6,6 +6,7 @@ import io.github.stainlessstasis.satiscraftory.recipe.BuildingCost;
 import io.github.stainlessstasis.satiscraftory.registry.SCItems;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.PackOutput;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.ItemLike;
 import org.jspecify.annotations.NonNull;
 
@@ -17,27 +18,53 @@ public class SCBuildingCostsProvider extends BuildingCostProvider {
 
     @Override
     protected void addCosts(CostOutput output) {
+        addCost(output, SCItems.MINER_MK1, BuildingCost.Data.builder(SCItems.MINER_MK1.get())
+                .input(SCItems.IRON_PLATE.get(), 10) // TODO: add concrete
+                .build());
+        addCost(output, SCItems.BELT_MK1, BuildingCost.Data.builder(SCItems.BELT_MK1.get())
+                .input(SCItems.IRON_PLATE.get(), 1)
+                .build());
+        addCost(output, SCItems.BELT_MK2, BuildingCost.Data.builder(SCItems.BELT_MK2.get())
+                .input(SCItems.IRON_PLATE.get(), 6) // TODO: replace with reinforced iron plate
+                .input(SCItems.SCREWS.get(), 12)
+                .build());
+        addCost(output, SCItems.BELT_MK3, BuildingCost.Data.builder(SCItems.BELT_MK3.get())
+                .input(Items.BEDROCK, 999) // TODO: replace with steel later, unobtainable for now
+                .build());
+        addCost(output, SCItems.POWER_POLE_MK1, BuildingCost.Data.builder(SCItems.POWER_POLE_MK1.get())
+                .input(SCItems.IRON_ROD.get(), 1) // TODO: add concrete
+                .input(SCItems.WIRE.get(), 3)
+                .build());
+        addCost(output, SCItems.BIOMASS_BURNER, BuildingCost.Data.builder(SCItems.BIOMASS_BURNER.get())
+                .input(SCItems.IRON_PLATE.get(), 15)
+                .input(SCItems.IRON_ROD.get(), 15)
+                .input(SCItems.WIRE.get(), 25)
+                .build());
+        addCost(output, ManifoldItems.SPLITTER, BuildingCost.Data.builder(ManifoldItems.SPLITTER.get())
+                .input(SCItems.IRON_PLATE.get(), 2)
+                .input(SCItems.CABLE.get(), 2)
+                .build());
         addCost(output, ManifoldItems.MERGER, BuildingCost.Data.builder(ManifoldItems.MERGER.get())
                 .input(SCItems.IRON_PLATE.get(), 2)
                 .input(SCItems.IRON_ROD.get(), 2)
                 .build());
-
-        addPlaceholder(output, SCItems.MINER_MK1);
-        addPlaceholder(output, SCItems.BELT_MK1);
-        addPlaceholder(output, SCItems.BELT_MK2);
-        addPlaceholder(output, SCItems.BELT_MK3);
-        addPlaceholder(output, SCItems.POWER_POLE_MK1);
-        addPlaceholder(output, SCItems.BIOMASS_BURNER);
-        addPlaceholder(output, ManifoldItems.SPLITTER);
-        addPlaceholder(output, ManifoldItems.MACHINE);
-        addPlaceholder(output, ManifoldItems.CONTAINER);
-        addPlaceholder(output, ManifoldItems.CONSUMER);
-        addPlaceholder(output, ManifoldItems.POWER_PRODUCER);
-    }
-
-    private void addPlaceholder(CostOutput output, ItemLike buildingItem) {
-        addCost(output, buildingItem, BuildingCost.Data.builder(buildingItem)
-                .input(SCItems.IRON_PLATE.get(), 1)
+        addCost(output, ManifoldItems.MACHINE, BuildingCost.Data.builder(ManifoldItems.MACHINE.get())
+                .input(SCItems.IRON_PLATE.get(), 8) // TODO: separate into smelter/constructor in 0.7.0 with their actual recipes
+                .input(SCItems.IRON_ROD.get(), 4)
+                .input(SCItems.WIRE.get(), 8)
+                .build());
+        addCost(output, ManifoldItems.CONTAINER, BuildingCost.Data.builder(ManifoldItems.CONTAINER.get())
+                .input(SCItems.IRON_PLATE.get(), 10)
+                .input(SCItems.IRON_ROD.get(), 10)
+                .build());
+        addCost(output, ManifoldItems.CONTAINER, BuildingCost.Data.builder(ManifoldItems.CONTAINER.get())
+                .input(SCItems.IRON_PLATE.get(), 10)
+                .input(SCItems.IRON_ROD.get(), 10)
+                .build());
+        addCost(output, ManifoldItems.CONSUMER, BuildingCost.Data.builder(ManifoldItems.CONSUMER.get()) // TODO: replace with awesome sink eventually
+                .input(SCItems.IRON_PLATE.get(), 15)
+                .input(SCItems.IRON_ROD.get(), 10)
+                .input(SCItems.WIRE.get(), 15)
                 .build());
     }
 
